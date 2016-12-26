163163cb65defb187eec54b314947185f135c372
jdnickell <jd@jdnickell.com>
2016-12-26 10:21:15 -0600

    add sql to create Users database, remove some usings

---
+++ MVC-AuthenticationTemplate/MVC-AuthenticationTemplate/SqlFiles/CreateAspNetUsers.sql
@@ -0,0 +1,32 @@
+USE [DevBlink]
+GO
+
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 12/26/2016 10:17:34 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [nvarchar](max) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEndDateUtc] [datetime] NOT NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailCount] [int] NOT NULL,
+	[UserName] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+
