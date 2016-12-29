d4aacc13b23b02f3dd19c0c9349786846e0aafa2
jdnickell <jd@jdnickell.com>
2016-12-29 16:19:14 -0600

    another initial commit stealing from the default MVC project without most unnecessary resources

---
+++ MVC-AuthenticationTemplate/MVC-AuthenticationTemplate/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace MVC_AuthenticationTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
